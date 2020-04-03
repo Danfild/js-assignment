13d6ccb1436c5e8b248fc3f7c910a9e31c1a780c
Ubuntu <ubuntu@ip-172-31-47-135.us-east-2.compute.internal>
2020-04-03 16:20:13 +0000

    NewNe

--- auth-lib.js
+++ auth-lib.js
@@ -1,8 +1,33 @@
-let allUsers = [
-	{nickname: "admin", password: "1234", groups: ["admin", "manager", "basic"]},
-	{nickname: "sobakajozhec", password: "ekh228", groups: ["basic", "manager"]},
-	{nickname: "patriot007", password: "russiaFTW", groups: ["basic"]}
-];
+// let allUsers = [
+// 	{nickname: "admin", password: "1234", groups: ["admin", "manager", "basic"]},
+// 	{nickname: "sobakajozhec", password: "ekh228", groups: ["basic", "manager"]},
+// 	{nickname: "patriot007", password: "russiaFTW", groups: ["basic"]}
+// ];
+let allUsers = {
+    "admin" : {password: "1234", groups: ["admin", "manager", "basic"]}
+};
+
+// let allGroups = {
+// 	"admin" : {"rights" :[rights[2]],"users" :["123"]},
+// 	"manager": {"rights" :[rights[1]], "users" :["1234"]},
+// 	"basic": {"rights" :[rights[3]], "users" :["12345"]}
+	
+// }
+// let allGroups = {
+// 	"admin" : {"rights" :[rights[2]],"users" :["123"]}
+	
+// }
+
+let allGroups = {
+	"admin": [rights[2]],
+	"manager": [rights[0]],
+	"basic": [rights[1], rights[3]]
+}
+
+
+
+let allRights = ["manage content", "play games", "delete users", "view site"];
+
 
 // Возвращает массив всех пользователей.
 function users() {
@@ -11,8 +36,9 @@ function users() {
 
 //Создает нового пользователя с указанным логином username и паролем password, возвращает созданного пользователя.
 function createUser(name, password) {
-    allUsers.push(name, password)
-    return name;
+    let user = {nickname: name, password: password};
+    allUsers.push(user)
+    return user;
 }
 
 // Удаляет пользователя user
@@ -21,27 +47,49 @@ function deleteUser(user) {
     
 }
 
-// Возвращает массив групп, к которым принадлежит пользователь user
+
+
+ //Возвращает массив групп, к которым принадлежит пользователь user
 function userGroups(user) {
-    return allUsers ;
+    return allGroups ;
 }
 
-// Добавляет пользователя user в группу group
-function addUserToGroup(user, group) {
+
+ // Добавляет пользователя user в группу group
+ function addUserToGroup(user, group) {
+    if (user && group){
+    //     allUsers.forEach((element) => {
+    //      console.log(element)
+    //      if (element.nickname == user){
+    //          element.groups.push(group)
+    //      }
+        
+    // })
+        allUsers.user.groups.push(group);
+    }else {
+        throw new Error();
+        
+    }
+     
+}
+
+
+
+// Удаляет пользователя user из группы group. Должна бросить исключение, если пользователя user нет в группе group
+function removeUserFromGroup(user, group) {
     
 }
 
-// Удаляет пользователя user из группы group. Должна бросить исключение, если пользователя user нет в группе group
-function removeUserFromGroup(user, group) {}
-
 // Возвращает массив прав
 function rights() {
-    let rights = [deleteUser, createUser];
-    return rights;
+     return allRights;
 }
 
 // Создает новое право с именем name и возвращает его
-function createRight(name) {}
+function createRight(name) {
+     allRights.push(name)
+     return name;
+}
 
 // Удаляет право right
 function deleteRight(right) {}
