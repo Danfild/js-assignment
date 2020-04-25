456f5e0908296e470d750d2e79c897f50b829f59
Danfild <62719747+Danfild@users.noreply.github.com>
2020-04-25 16:01:54 +0500

    Update auth-lib.js
    mew

--- auth-lib.js
+++ auth-lib.js
@@ -1,33 +1,18 @@
-// let allUsers = [
-// 	{nickname: "admin", password: "1234", groups: ["admin", "manager", "basic"]},
-// 	{nickname: "sobakajozhec", password: "ekh228", groups: ["basic", "manager"]},
-// 	{nickname: "patriot007", password: "russiaFTW", groups: ["basic"]}
-// ];
-let allUsers = {
-    "admin" : {password: "1234", groups: ["admin", "manager", "basic"]}
-};
+let allUsers = [
+    {nickname: "admin", password: "1234", groups: ["admin", "manager", "basic"]},
+    {nickname: "sobakajozhec", password: "ekh228", groups: ["basic", "manager"]},
+    {nickname: "patriot007", password: "russiaFTW", groups: ["basic"]}
+];
 
-// let allGroups = {
-// 	"admin" : {"rights" :[rights[2]],"users" :["123"]},
-// 	"manager": {"rights" :[rights[1]], "users" :["1234"]},
-// 	"basic": {"rights" :[rights[3]], "users" :["12345"]}
-	
-// }
-// let allGroups = {
-// 	"admin" : {"rights" :[rights[2]],"users" :["123"]}
-	
-// }
+let allRights = [
+"manage content", "play games", "delete users", "view site"
+];
 
-let allGroups = {
-	"admin": [rights[2]],
-	"manager": [rights[0]],
-	"basic": [rights[1], rights[3]]
-}
-
-
-
-let allRights = ["manage content", "play games", "delete users", "view site"];
-
+let  allGroups = [
+    {name: "admin", rights: [allRights[2]]},
+    {name: "manager", rights: [allRights[0]]},
+    {name: "basic", rights: [allRights[1], allRights[3]]}
+]
 
 // Возвращает массив всех пользователей.
 function users() {
@@ -43,46 +28,50 @@ function createUser(name, password) {
 
 // Удаляет пользователя user
 function deleteUser(user) {
-   allUsers.splice(allUsers.indexOf(user));
-    
+    if (!user) {
+        throw Error("Pusto");
+    }
+    if (allUsers.indexOf(user) === -1){
+        throw Error("111");
+    }
+  allUsers.splice(allUsers.indexOf(user), 1);
+
 }
-
-
 
  //Возвращает массив групп, к которым принадлежит пользователь user
 function userGroups(user) {
-    return allGroups ;
+    return allGroups;
 }
 
 
  // Добавляет пользователя user в группу group
- function addUserToGroup(user, group) {
-    if (user && group){
-    //     allUsers.forEach((element) => {
-    //      console.log(element)
-    //      if (element.nickname == user){
-    //          element.groups.push(group)
-    //      }
-        
-    // })
-        allUsers.user.groups.push(group);
-    }else {
-        throw new Error();
-        
+ function addUserToGroup(user, group) { 
+ 	console.log(user);
+    if (!user || !group) {
+        throw Error("exception. undefined value");
     }
-     
-}
+    if (!allUsers.indexOf(user)) {
+        throw Error("exception. undefined");
+    }
+    if (!allGroups.indexOf(group)){
+        throw Error("exception. undefined");
+    }
+    allUsers.push(user,group);
 
+    // allUsers.indexOf(user).groups.push(group);
 
+  }
 
 // Удаляет пользователя user из группы group. Должна бросить исключение, если пользователя user нет в группе group
 function removeUserFromGroup(user, group) {
-    
+    allUsers.splice(allGroups.indexOf(user));
+    if (!user || !group) {throw Error("неверно");}
+
 }
 
 // Возвращает массив прав
-function rights() {
-     return allRights;
+function rights(right) {
+    return allRights;
 }
 
 // Создает новое право с именем name и возвращает его
@@ -92,25 +81,62 @@ function createRight(name) {
 }
 
 // Удаляет право right
-function deleteRight(right) {}
+function deleteRight(right) {
+	let index = allRights.indexOf(right);
+    
+     if (!right) {
+        throw Error("Pusto");
+    }
+     if (index == -1) {
+        throw Error("Pusto");
+    }
+        allRights.splice(index);
+}
 
 // Возвращает массив групп
-function groups() {}
+function groups() {
+    return allGroups;
+}
 
 // Создает новую группу и возвращает её.
-function createGroup(name) {}
+function createGroup(name) { 
+    allGroups.push({name:name,rights:[]});
+    return allGroups[allGroups.length-1];
+
+}
 
 // Удаляет группу group
-function deleteGroup(group) {}
+function deleteGroup(group) {
+    allGroups.splice(allGroups.indexOf(group), 1);
+//     if (!group != group){throw Error("fail");}
+}
 
 // Возвращает массив прав, которые принадлежат группе group
-function groupRights() {}
+function groupRights(group) {
+    return allRights;
+}
 
 // Добавляет право right к группе group
-function addRightToGroup() {}
+function addRightToGroup(right,group) {
+
+    
+}
 
 // Удаляет право right из группы group. Должна бросить исключение, если права right нет в группе group
-function removeRightFromGroup() {}
+function removeRightFromGroup(right,group) {
+	// let index = allGroups.indexOf(group);
+    
+ //     if (index == -1) {
+ //        throw Error("Pusto");
+ //    	}
+ //    // allRights.splice(allGroups.indexOf(rights
+ //    // allGroups.getValue(index);
+ //    let group1 = allGroups[index];
+ //    // group.rights.splice()
+    console.log(group);
+    allGroups.rights.splice(group.rights.indexOf(right));
+    console.log(group);
+}
 
 function login(username, password) {}
 
